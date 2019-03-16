0f4f595cffef2bc6cac44e1d9ab9ddb5ad4a7385
zixuannnn <48137765+zixuannnn@users.noreply.github.com>
2019-03-16 18:43:02 -0300

    Add files via upload

--- Experiment.py
+++ Experiment.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request_started, request_finished
 import time
 
 app = Flask(__name__)
@@ -6,19 +6,34 @@ app = Flask(__name__)
 @app.before_request
 def start_request():
     g.start = time.time()
-    print("before request: ", g.start)
 
 @app.after_request
 def end_request(responce):
-    endtime = time.time()
-    time_diff = endtime - g.start
-    print("after request", endtime)
-    print("Web Service total delay: ", time_diff)
+    g.endtime = time.time()
+    g.exediff = g.endtime - g.start
+    print("Execution delay: ", g.exediff)
     return responce
+
+def requeststarted(sender, **extra):
+    g.started = time.time()
+    return "start"
+
+request_started.connect(requeststarted,app)
+
+def requestfinish(sender, response, **extra):
+    endtime = time.time()
+    g.totaldiff = endtime-g.started
+    g.transitdiff = g.totaldiff-(g.endtime-g.start)
+    print("Total Delay: ", g.totaldiff)
+    print("Network Transit Delay: ", g.transitdiff)
+
+    return "finish"
+
+request_finished.connect(requestfinish,app)
 
 @app.route('/')
 def run():
-    return render_template('page.html')
+    return render_template('page.html', totaldelay=str(g.totaldiff), executiontime=str(g.exediff), networkdelay=str(g.transitdiff))
 
 if __name__ == '__main__':
     app.run(debug=True)
